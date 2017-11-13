a4636d1b71bcf245f89938aeb9e8a896070779c4
Nikolas Martens <Nikolas.M@rtens.org>
2017-11-13 08:21:52 +0100

    Emitter buttons, display controls, adaptive display size, multiple displays

--- python/src/zells/TurtleZell.py
+++ python/src/zells/TurtleZell.py
@@ -4,7 +4,8 @@ import math
 class TurtleZell(object):
     position = [0, 0]
     angle = 90
-    canvas = None
+    segments = []
+    canvases = {}
 
     def __init__(self, name, emit):
         self.name = name
@@ -15,14 +16,14 @@ class TurtleZell(object):
             return
 
         if 'drawOn' in signal['content']:
-            self.canvas = signal['content']['drawOn']
+            self.canvases[signal['content']['drawOn']] = signal['content']['size']
             self.draw()
 
         if 'go' in signal['content']:
             first = [self.position[0], self.position[1]]
-            self.position[0] += math.cos(math.radians(self.angle)) * 10
-            self.position[1] += math.sin(math.radians(self.angle)) * 10
-            self.drawSegment(first, self.position)
+            self.position[0] += math.cos(math.radians(self.angle))
+            self.position[1] += math.sin(math.radians(self.angle))
+            self.segments.append([first, [self.position[0], self.position[1]]])
             self.draw()
 
         if 'turn' in signal['content']:
@@ -32,38 +33,42 @@ class TurtleZell(object):
                 self.angle += 15
             self.draw()
 
-    def drawSegment(self, p1, p2):
-        self.emit({
-            'to': self.canvas,
-            'content': {'draw': {
-                'layer': 0,
-                'strokes': [{'line': {'from': {'x': p1[0], 'y': p1[1]},
-                                      'to': {'x': p2[0], 'y': p2[1]},
-                                      'color': {'r': 1, 'g': 1, 'b': 1},
-                                      'width': 1}}]}}})
-
     def draw(self):
-        point1x = self.position[0] + math.cos(math.radians(self.angle + 90)) * 10
-        point1y = self.position[1] + math.sin(math.radians(self.angle + 90)) * 10
-        point2x = self.position[0] + math.cos(math.radians(self.angle - 90)) * 10
-        point2y = self.position[1] + math.sin(math.radians(self.angle - 90)) * 10
-        point3x = self.position[0] + math.cos(math.radians(self.angle + 0)) * 20
-        point3y = self.position[1] + math.sin(math.radians(self.angle + 0)) * 20
+        for canvas in self.canvases:
 
-        self.emit({
-            'to': self.canvas,
-            'content': {'draw': {
-                'layer': 1,
-                'strokes': ['clear',
-                            {'line': {'from': {'x': point1x, 'y': point1y},
-                                      'to': {'x': point2x, 'y': point2y},
-                                      'color': {'r': 0, 'g': 1, 'b': 0},
-                                      'width': 1}},
-                            {'line': {'from': {'x': point2x, 'y': point2y},
-                                      'to': {'x': point3x, 'y': point3y},
-                                      'color': {'r': 0, 'g': 1, 'b': 0},
-                                      'width': 1}},
-                            {'line': {'from': {'x': point3x, 'y': point3y},
-                                      'to': {'x': point1x, 'y': point1y},
-                                      'color': {'r': 0, 'g': 1, 'b': 0},
-                                      'width': 1}}]}}})
+            size = self.canvases[canvas]
+            stepX = size['width'] / 100
+            stepY = size['height'] / 100
+
+            point1x = self.position[0] * stepX + math.cos(math.radians(self.angle + 90)) * 10
+            point1y = self.position[1] * stepY + math.sin(math.radians(self.angle + 90)) * 10
+            point2x = self.position[0] * stepX + math.cos(math.radians(self.angle - 90)) * 10
+            point2y = self.position[1] * stepY + math.sin(math.radians(self.angle - 90)) * 10
+            point3x = self.position[0] * stepX + math.cos(math.radians(self.angle + 0)) * 20
+            point3y = self.position[1] * stepY + math.sin(math.radians(self.angle + 0)) * 20
+
+            strokes = []
+            strokes.append('clear')
+
+            for segment in self.segments:
+                strokes.append({'line': {'from': {'x': segment[0][0] * stepX, 'y': segment[0][1] * stepY},
+                                         'to': {'x': segment[1][0] * stepX, 'y': segment[1][1] * stepY},
+                                         'color': {'r': 1, 'g': 1, 'b': 1},
+                                         'width': 1}})
+
+            strokes.append({'line': {'from': {'x': point1x, 'y': point1y},
+                                     'to': {'x': point2x, 'y': point2y},
+                                     'color': {'r': 0, 'g': 1, 'b': 0},
+                                     'width': 1}})
+            strokes.append({'line': {'from': {'x': point2x, 'y': point2y},
+                                     'to': {'x': point3x, 'y': point3y},
+                                     'color': {'r': 0, 'g': 1, 'b': 0},
+                                     'width': 1}})
+            strokes.append({'line': {'from': {'x': point3x, 'y': point3y},
+                                     'to': {'x': point1x, 'y': point1y},
+                                     'color': {'r': 0, 'g': 1, 'b': 0},
+                                     'width': 1}})
+
+            self.emit({
+                'to': canvas,
+                'content': {'draw': {'strokes': strokes}}})
