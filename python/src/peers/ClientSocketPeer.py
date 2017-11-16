590a23785d5302a3c8917770ead1b328aade3dd7
Nikolas Martens <Nikolas.M@rtens.org>
2017-11-16 12:45:14 +0100

    Read transmissions from stream [closes #1]

--- python/src/peers/ClientSocketPeer.py
+++ python/src/peers/ClientSocketPeer.py
@@ -3,6 +3,7 @@ import select
 import msgpack
 from thread import start_new_thread
 
+
 class ClientSocketPeer:
     whenReceive = lambda: None
     whenClose = lambda: None
@@ -11,14 +12,12 @@ class ClientSocketPeer:
         print "connect " + host + ":" + str(port)
 
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.settimeout(2)
 
         try:
             self.socket.connect((host, port))
+            start_new_thread(self.thread, ())
         except:
             print 'Unable to connect'
-
-        start_new_thread(self.thread, ())
 
     def onReceive(self, then):
         self.whenReceive = then
@@ -30,22 +29,14 @@ class ClientSocketPeer:
         self.socket.send(msgpack.packb(['transmit', id, signal]))
 
     def thread(self):
-        while 1:
-            ready_to_read, ready_to_write, in_error = select.select([self.socket], [], [])
+        unpacker = msgpack.Unpacker()
 
-            for sock in ready_to_read:
-                if sock == self.socket:
-                    # incoming message from remote server, s
-                    data = sock.recv(4096)
-                    if not data:
-                        self.whenClose()
-                        return
-                    else:
-                        transmission = msgpack.unpackb(data)
-
-                        if transmission[0] == 'join':
-                            pass
-                        if transmission[0] == 'transmit':
-                            self.whenReceive(transmission[1], transmission[2])
-                        else:
-                            print "Unknown transmission"
+        while True:
+            unpacker.feed(self.socket.recv(1))
+            for transmission in unpacker:
+                if transmission[0] == 'join':
+                    pass
+                if transmission[0] == 'transmit':
+                    self.whenReceive(transmission[1], transmission[2])
+                else:
+                    print "Unknown transmission"
