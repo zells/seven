a743eb82616aa8e57b23e131e018eb3f05fc2620
Nikolas Martens <Nikolas.M@rtens.org>
2017-12-12 08:33:07 +0100

    Communicator: print single bytes as characters

--- javascript/web/communicator.js
+++ javascript/web/communicator.js
@@ -166,7 +166,7 @@ function print(signal, indent) {
         return '\n' + indent + '[' + signal.map(function (s) {
                 return print(s, indent + ' ');
             }).join(',').trim() + ']';
-    } else if (signal.length > 1 && Math.min.apply(null, signal) > 31 && Math.max.apply(null, signal) < 127) {
+    } else if (Math.min.apply(null, signal) > 31 && Math.max.apply(null, signal) < 127) {
         return '"' + signal.toString() + '"';
     } else {
         return encoding.translate.toNumber(signal).toString()
